30d93367d634d8418823d53858b3b83176813cdf
Pharnuwat <se56160411@gmail.com>
2025-08-21 16:22:29 +0700

    edit: delet page

--- client/src/components/InputText.jsx
+++ client/src/components/InputText.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import ViewTravel from "../page/ViewTravel";
+import ViewTravel from "./ViewTravel";
 
 function InputText() {
   const [searchResult, setSearchResult] = useState([]);
